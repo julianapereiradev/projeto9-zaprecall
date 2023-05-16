f7cb54ab5a79bec68a4bfb00e15f26ce97acfcbb
juliana <julianapereira.dev@gmail.com>
2023-05-16 14:25:34 -0300

    Instalação e teste funcionando do styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledButtonBackground = styled.button`
+background: blue;
+color: #FFFFFF
+`
 
 export default function App() {
 
   return (
     <div>
       <h3>Hello World!</h3>
+      <StyledButtonBackground>Teste cores</StyledButtonBackground>
     </div>
   )
 }
